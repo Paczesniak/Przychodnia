c7a30bc8a5e7a4a6798286718604154f5dddd7ae
bartek <barek.paczesniak@onet.pl>
2025-04-05 17:25:38 +0200

    Fix mixed content by using secure_asset for stylesheets

--- resources/views/choroby/edit.blade.php
+++ resources/views/choroby/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edytuj Chorobę</title>
-    <link rel="stylesheet" href="{{ asset('css_login.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css_login.css') }}">
 </head>
 <body>
     <div class="login-box">
--- resources/views/lekarze/show.blade.php
+++ resources/views/lekarze/show.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Szczegóły lekarza</title>
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/custom.css') }}">
     <style>
         html {
             height: 100%;
